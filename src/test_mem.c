1b7930ccd22aaf89583f3c834394e262eacd1674
Noor Schat <nschat@student.codam.nl>
2019-11-02 21:08:30 +0100

    feat: add memcpy tests, update libft

--- src/test_mem.c
+++ src/test_mem.c
@@ -6,7 +6,7 @@
 /*   By: nschat <nschat@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/10/30 13:11:32 by nschat        #+#    #+#                 */
-/*   Updated: 2019/11/02 20:17:03 by nschat        ########   odam.nl         */
+/*   Updated: 2019/11/02 21:08:12 by nschat        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,14 @@ Test(mem, ft_bzero)
 	ft_bzero(a, 8);
 	bzero(b, 8);
 	cr_expect_arr_eq(a, b, 12);
+}
+
+Test(mem, ft_memcpy)
+{
+	char	a[] = "\0ajskdasjldki\0uvnadhipsrl";
+	char	b[] = "\0ajskdasjldki\0uvnadhipsrl";
+
+	cr_expect_arr_eq(ft_memcpy(a, "19274128390", 12), memcpy(b, "19274128390", 12), 12);
+	cr_expect_arr_eq(ft_memcpy(a, "", 12), memcpy(b, "", 12), 12);
+	cr_expect_eq(ft_memcpy(NULL, NULL, 12), memcpy(NULL, NULL, 12));
 }
